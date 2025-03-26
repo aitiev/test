bbde7b213d03dff260b7a27c689dc345a9c6b3c6
Munarbek Aitiev <munarbek.aitiev@gmail.com>
2025-03-26 06:38:27 +0600

    setting up django

--- a.py
+++
@@ -1,7 +0,0 @@
-# Adding merge conflict Lines Changes first line
-# First Testing Line Changing to imitate merge conflict
-print("Hellow World!")
-
-# Second Testing Line
-
-# Some other changes also changed
